2245687ce0a70bdc117d083ae713b815aa8ce9b1
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2021-05-17 11:20:48 +0700

    Remove Unused Code

--- app/src/main/java/com/fyndev/moviecatalogue/home/favorite/movie/FavoriteMovieViewModel.kt
+++ app/src/main/java/com/fyndev/moviecatalogue/home/favorite/movie/FavoriteMovieViewModel.kt
@@ -8,11 +8,6 @@ import com.fyndev.moviecatalogue.data.source.local.entity.MovieEntity
 
 class FavoriteMovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
 
-    fun setFavoriteMovie(movie: MovieEntity) {
-        val newState = !movie.isFavorite
-        movieRepository.setFavoriteMovie(movie, newState)
-    }
-
     fun getFavoriteMovie(): LiveData<PagedList<MovieEntity>> {
         return movieRepository.getFavoriteMovie()
     }
--- app/src/main/java/com/fyndev/moviecatalogue/home/favorite/tvshow/FavoriteTvShowViewModel.kt
+++ app/src/main/java/com/fyndev/moviecatalogue/home/favorite/tvshow/FavoriteTvShowViewModel.kt
@@ -8,11 +8,6 @@ import com.fyndev.moviecatalogue.data.source.local.entity.TvShowEntity
 
 class FavoriteTvShowViewModel(private val movieRepository: MovieRepository) : ViewModel() {
 
-    fun setFavoriteTvShow(tvShow: TvShowEntity) {
-        val newState = !tvShow.isFavorite
-        movieRepository.setFavoriteTvShow(tvShow, newState)
-    }
-
     fun getFavoriteTvShow(): LiveData<PagedList<TvShowEntity>> {
         return movieRepository.getFavoriteTvShow()
     }
